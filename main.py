424239e3c4620da85c3ee91429dc43c191ab9641
Tashdacat <tashdacat@gmail.com>
2025-08-05 12:57:12 +1000

    Updated main to retrieve text from books

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as book:
+        book_contents = book.read()
+    return book_contents
+
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+    print(contents)
+
+main()
